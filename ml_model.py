1fa709bc9f9765d76af6a4d35cdd338aff944f14
saketprusty <jsaketprusty@gmail.com>
2023-02-25 12:06:54 +0530

    added 3 ML modelsss

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ml related code")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model1():
+    print("adding code for ML Model2")
+
+def model1():
+    print("adding code for ML Model3")    
